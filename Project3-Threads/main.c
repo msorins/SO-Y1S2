96efd42cf2796d372e744ec7e87832dd272cd608
Mircea <sorynsoo@gmail.com>
2017-04-27 15:55:22 +0300

    Project3 almost

---
+++ Project3-Threads/main.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <ctype.h>
+
+#define MAXSIR 100
+
+
+pthread_mutex_t countAppearencesMutex;
+int appearences;
+
+//Struct for transferring information to thread
+typedef struct dataTransferThread{
+    char file[MAXSIR];
+    char word[MAXSIR];
+};
+
+
+//Thread computing function
+void* computeFileThread(void *arg) {
+    struct dataTransferThread *obj = (struct dataTransferThread*)arg;
+    printf("Thread. File: %s\n Words: %s\n", obj->file, obj->word);
+
+    //Open the file
+    FILE *file = fopen(obj->file, "r");
+    char crtChar, lastChar=NULL;
+    int nrCuv = 0;
+
+    //Check to see if file really opened
+    if(!file) {
+        perror("Could not open the file");
+        exit(5);
+    }
+
+    //Count the words
+    while((crtChar = getc(file)) != EOF) {
+        if((crtChar == ' ' || crtChar =='\n') && (isalpha(lastChar) || lastChar == NULL ))
+            nrCuv ++;
+
+        lastChar = crtChar;
+    }
+
+    printf("nrcuv: %d\n", nrCuv);
+
+    //Add current result to a global one (synchronised operation)
+    pthread_mutex_lock(&countAppearencesMutex);
+    appearences += nrCuv;
+    pthread_mutex_unlock(&countAppearencesMutex);
+
+
+    return 0;
+}
+
+
+
+int main(int argc, char *argv[]) {
+    //Initialisaton
+    int actualArgc = argc -1;
+    int i;
+    char file[100];
+    pthread_t threads[100];
+
+    //Arguments validation
+    if(actualArgc < 2) {
+        perror("Must be minimum 2 arguments");
+        exit(1);
+    }
+
+    //Save the first argument as the file
+    strcpy(file, argv[1]);
+
+    //Initialise MUTEX
+    if (pthread_mutex_init(&countAppearencesMutex, NULL) != 0)
+    {
+        perror("Mutex init failed");
+        exit(2);
+    }
+
+    //Create the threads
+    for(i = 2; i < argc; i++) {
+        //Create the transfer object (to free it at the end of thread)
+        struct dataTransferThread* obj = malloc(sizeof(struct dataTransferThread));
+        strcpy(obj->file, file);
+        strcpy(obj->word, argv[i]);
+
+        //Start that thread
+        pthread_create(&threads[i], NULL, computeFileThread, obj);
+    }
+
+    //Save the threads
+    for(i = 2; i < argc; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    //Just print the result
+    printf("Number of total appearences: %d \n", appearences);
+
+    //Destroy MUTEX
+    pthread_mutex_destroy(&countAppearencesMutex);
+
+    return 0;
+}
